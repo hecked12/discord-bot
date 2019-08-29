6a08ebb75e90a2bfaa9be6e440d2933bdc7db227
A.N <spidernet12@gmail.com>
2019-08-29 22:10:20 +0300

    First command
    simple test

---
+++ dist/commands/testCommand.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class testCommand {
+    constructor() {
+        this._command = "testCommand";
+    }
+    help() {
+        return "This is a test command for bot.";
+    }
+    isThisCommand(command) {
+        return command === this._command;
+    }
+    runCommand(args, msgObject, client) {
+        msgObject.channel.send("Working!");
+    }
+}
+exports.default = testCommand;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdENvbW1hbmQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29tbWFuZHMvdGVzdENvbW1hbmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFHQSxNQUFxQixXQUFXO0lBQWhDO1FBQ3FCLGFBQVEsR0FBRyxhQUFhLENBQUM7SUFZOUMsQ0FBQztJQVZHLElBQUk7UUFDQSxPQUFPLGlDQUFpQyxDQUFDO0lBQzdDLENBQUM7SUFFRCxhQUFhLENBQUMsT0FBZTtRQUN6QixPQUFPLE9BQU8sS0FBSyxJQUFJLENBQUMsUUFBUSxDQUFDO0lBQ3JDLENBQUM7SUFDRCxVQUFVLENBQUMsSUFBYyxFQUFFLFNBQTBCLEVBQUUsTUFBc0I7UUFDekUsU0FBUyxDQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsVUFBVSxDQUFDLENBQUM7SUFDdkMsQ0FBQztDQUNKO0FBYkQsOEJBYUMifQ==
